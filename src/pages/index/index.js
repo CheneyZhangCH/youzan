3d819f6ae4f6d96e7284138efb2bc1ad867fccfc
cheney zhang <cheney_zhang@foxmail.com>
2018-03-04 23:29:08 +0800

    mint实现推荐商品的懒加载

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,18 +5,49 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 
+
+import {InfiniteScroll} from 'mint-ui';
+
+Vue.use(InfiniteScroll);
+
+
 let app = new Vue({
   el: '#app',
   data: {
-    lists: null
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false,
+    allLoaded: false
   },
   created() {
-    axios.post(url.hostLists, {
-      pageNum: 1,
-      pageSize: 6
-    }).then((res) => {
-      this.lists = res.data[0].lists
-    })
-
+    this.getLists()
+  },
+  methods: {
+    getLists() {
+      if (this.allLoaded) {
+        return
+      }
+      this.loading = true
+      axios.post(url.hostLists, {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then((res) => {
+        let currentLists = res.data[0].lists
+        //判断数据是否加载完毕
+        if (currentLists.length < this.pageSize) {
+          this.allLoaded = true
+        }
+        if (this.lists) {
+          // 已有数据再次请求
+          this.lists = this.lists.concat(currentLists)
+        } else {
+          // 初次请求
+          this.lists = res.data[0].lists
+        }
+        this.loading = false
+        this.pageNum++
+      })
+    }
   }
 })
